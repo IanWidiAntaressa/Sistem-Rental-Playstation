f04a3e4f0e903320c18e10bfc98aeaf19ec117d3
ptrwwjyfz <pwijaya928@gmailcom>
2026-05-05 05:07:48 +0700

    rev error

--- server.js
+++ server.js
@@ -44,3 +44,5 @@ app.get('/', (req, res) => res.redirect('/auth/login'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server jalan di http://localhost:${PORT}`));
+
+module.exports = app;
